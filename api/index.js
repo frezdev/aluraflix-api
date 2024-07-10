dd23cab6b264ecd158524428ae0fa355f7445463
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-07-09 21:53:43 -0500

    Add cors origin

--- api/index.js
+++ api/index.js
@@ -13,7 +13,12 @@ const app = express()
 const PORT = env.PORT ?? 3002
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://aluraflix-frezdev.vercel.app/'
+  ]
+}))
 
 app.get('/videos', async (req, res) => {
   try {
